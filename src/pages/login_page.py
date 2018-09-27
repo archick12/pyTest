821e97552e779f50ab73654dfd224b8cfbd9b51d
apiliuk <lohika.artur@98point6.com>
2018-09-27 15:14:55 +0300

    added allure reports

--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,6 +13,7 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
+    @allure.step
     def login_to_jira(self):
         self.driver.find_element(*self.LOGIN_INPUT).clear()
         self.driver.find_element(*self.LOGIN_INPUT).send_keys("webinar5")
@@ -19,12 +21,15 @@ class LoginPage:
         self.driver.find_element(*self.PASSWORD_INPUT).send_keys("webinar5")
         self.driver.find_element(*self.LOGIN_BUTTON).submit()
 
+    @allure.step
     def click_login_button(self):
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(*self.LOGIN_INPUT)).click()
 
+    @allure.step
     def at_page(self):
         return "System Dashboard - Hillel IT School JIRA" in self.driver.title
 
+    @allure.step
     def open(self):
         self.driver.get("http://jira.hillel.it:8080/secure/Dashboard.jspa")
         return self
--- tests/ui/test_pytest_markers.py
+++ tests/ui/test_pytest_markers.py
@@ -1,3 +1,6 @@
+import random
+
+import allure
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -12,14 +15,26 @@ class TestLogin:
         self.login_page = LoginPage(self.driver)
         self.header_page = HeaderPage(self.driver)
 
+    @allure.title("Create issue UI")
     @pytest.mark.regression
     @pytest.mark.P1
-    @pytest.mark.skip(reason="NMB-12222 User authorization is blocked by advertisement")
     def test_login_to_jira_page_object(self):
         self.login_page.open()
         assert self.login_page.at_page()
         self.login_page.login_to_jira()
         assert self.header_page.at_page()
+
+    @allure.title("Test to be skipped")
+    @pytest.mark.feature
+    @pytest.mark.skip(reason="NMB-12222 User authorization is blocked by advertisement")
+    def test_to_be_skipped(self):
+        # TODO: implement this test
+        pass
+
+    @allure.title("Unstable test")
+    @pytest.mark.flaky(reruns=3, reruns_delay=1)
+    def test_unstable(self):
+        assert random.randint(1, 2) == 2
 
     def teardown_method(self):
         self.driver.close()
